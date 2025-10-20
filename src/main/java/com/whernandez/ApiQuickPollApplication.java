83f4b12c23c7f4812e57ee61acbdf997988ac5c2
williamhdz94 <williamhernandez450@gmail.com>
2025-10-20 07:26:34 -0600

    Configuration project and firsts entities

---
+++ src/main/java/com/whernandez/ApiQuickPollApplication.java
@@ -0,0 +1,13 @@
+package com.whernandez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiQuickPollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiQuickPollApplication.class, args);
+	}
+
+}
