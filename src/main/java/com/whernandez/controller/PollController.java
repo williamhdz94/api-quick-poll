7bd857b9d8c6ca002d0266d52490975535d1c782
williamhdz94 <williamhernandez450@gmail.com>
2025-10-20 19:59:34 -0600

    Add controller Poll and CRUD operations

--- src/main/java/com/whernandez/controller/PollController.java
+++ src/main/java/com/whernandez/controller/PollController.java
@@ -1,14 +1,18 @@
 package com.whernandez.controller;
 
 import java.net.URI;
+import java.util.Optional;
 
 import javax.inject.Inject;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -42,6 +46,29 @@ public class PollController {
 		resHeaders.setLocation(newPollUri);
 		
 		return new ResponseEntity<>(null, resHeaders, HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/polls/{pollId}")
+	public ResponseEntity<?> getPoll(@PathVariable Long pollId) throws Exception {
+		Optional<Poll> pollOptional = pollRepository.findById(pollId);
+		if ( !pollOptional.isPresent() ) {
+			throw new Exception("Poll not found");
+		}
+		
+		return new ResponseEntity<>(pollOptional.get(), HttpStatus.OK);
+	}
+	
+	@PutMapping("/polls/{pollId}")
+	public ResponseEntity<?> updatePollEntity(@RequestBody Poll poll, @PathVariable Long pollId) {
+		// Save the entity
+		Poll newPoll = pollRepository.save(poll);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/polls/{pollId}")
+	public ResponseEntity<?> deletePoll(@PathVariable Long pollId) {
+		pollRepository.deleteById(pollId);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 	
 }
